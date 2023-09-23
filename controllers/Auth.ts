78b1eae75b60e8d33fb7ec9c290119d3f7e1b1a1
sahilq312 <120485501+sahilq312@users.noreply.github.com>
2023-09-23 17:46:03 +0530

    commit

--- controllers/Auth.ts
+++ controllers/Auth.ts
@@ -23,15 +23,16 @@ export const register = async (req: Request, res: Response) => {
             })
             //saving data in database
             await user.save()
-            
+
             //creating token
-            let secret : string = process.env.JWT_SECRET as string;
-            const token = jwt.sign({id:user.id}, secret, {expiresIn: '2d'})
+            let secret: string = process.env.JWT_SECRET as string;
+            const token = jwt.sign({ id: user.id }, secret, { expiresIn: '2d' })
             //send data in response
             res
-                .cookie("jwt", token ,{
-                    httpOnly : true,
-                    expires : new Date(Date.now() + 24*60*60*1000)
+                .cookie('jwt', token, {
+                    httpOnly: true,
+                    expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+                    secure: true
                 })
                 .status(200)
                 .json(token)
@@ -45,31 +46,39 @@ export const register = async (req: Request, res: Response) => {
 
 export const login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
+
     try {
-        //if any email exists
-        const user = await User.findOne({ email: req.body.email })
-        if (!user) return res.status(400).json('email is wrong')
+        // Find the user by email
+        const user = await User.findOne({ email });
 
+        if (!user) {
+            return res.status(400).json({ error: 'Email is not registered' });
+        }
 
-        //checking password
-        const validpass = await bcrypt.compare(req.body.password, user.password)
-        if (!validpass) return res.status(400).json('invalid password')
+        // Check the password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
 
-        //create token
-        let secret : string = process.env.JWT_SECRET as string;
-        const token = jwt.sign({id:user.id}, secret, {expiresIn: '2d'})
-        //send response
-        res.cookie("token" , token, {
+        if (!isPasswordValid) {
+            return res.status(401).json({ error: 'Invalid password' });
+        }
+
+        // Create a JWT token
+        const secret: string = process.env.JWT_SECRET as string;
+        const token = jwt.sign({ id: user.id }, secret, { expiresIn: '2d' });
+
+        // Set the JWT token as an HttpOnly cookie
+        res.cookie('token', token, {
             httpOnly: true,
-            expires : new Date(Date.now()+ 24*60*60*1000)
-        })
-        .json({id : user.id})
-        .status(200).json("logged in")
+            expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+        });
+
+        // Send a success response with user ID
+        res.status(200).json({ id: user.id });
     } catch (error) {
-        console.log(error)
-        res.status(500).send('internal server error')
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
     }
-}
+};
 
 export const logout = async (req: Request, res: Response) => {
     try {
